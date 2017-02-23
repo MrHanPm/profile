11df7cc8bcdd9c4a6b07f4a07ef111056a52e8c6
pumpe zhu <zzplxm1988@gmail.com>
2017-02-23 18:28:57 +0800

    v0.1.1

--- src/components/app-footer.vue
+++ src/components/app-footer.vue
@@ -1,22 +1,19 @@
 <template>
-  <div class="footer">
-      <text class="text">客服电话：400-800-1616</text>
-  </div>
+  	<div class="footer">
+		<text class="footer-text">客服电话：400-800-1616</text>
+	</div>
 </template>
 
 <style scoped>
-  .footer {
-    position: relative;
-    height: 88px;
-    padding-left:120px;
-    padding-right: 120px;
-    background-color: #fff;
-    flex-direction: row;
-    flex-wrap: nowrap;
-    justify-content: flex-start;
-    align-items: center;
-  }
-  .text{
-
-  }
+.footer{
+	margin-top:30px;
+	margin-bottom: 30px;
+	height:24px;
+}
+.footer-text{
+	font-size: 24px;
+	line-height: 24px;
+	color:#2571E5;
+	text-align:center;
+}
 </style>
--- src/components/app-header.vue
+++ src/components/app-header.vue
@@ -1,14 +1,18 @@
 <template>
   <div class="header">
-      <div class="back" @click="jump('/')">
+      <div class="back">
         <text class="back-text">返回</text>
       </div>
-      <text class="title">我的消息</text>
-      <div class="clear" @click="clear">
-        <text>清空</text>   
-      </div>
+      <text class="title">{{title}}</text>
   </div>
 </template>
+
+<script>
+  export default {
+    name:'app-header',
+    props:['title']
+  }  
+</script>
 
 <style scoped>
   .header {
@@ -21,7 +25,9 @@
     flex-wrap: nowrap;
     justify-content: flex-start;
     align-items: center;
-    
+    border-bottom-width:1px;
+    border-bottom-style:solid;
+    border-bottom-color:#e3eaf7;
   }
   .back{
     position: absolute;
@@ -30,15 +36,16 @@
     width: 120px;
   }
   .back-text{
+    padding-left:36px;
     line-height: 88px;
-    text-align: center;
-    font-family: "Microsoft YaHei"
+    font-size: 28px;
+    color:#666;
   }
   .title {
     flex:1;
     line-height: 88px;
     color:#333;
+    font-size: 32px;
     text-align: center;
-    font-family: "Microsoft YaHei"
   }
 </style>
